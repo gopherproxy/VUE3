f611f2d65297d2fcdb21827176b8dc93a19fb96b
Marc <gopherproxy@gmail.com>
2021-10-04 15:07:55 +0200

    app.js

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ const app = Vue.createApp({
     //template: '<h2>I am the template now</h2>'
     data() {
         return {
-            title: 'Jurassic Park',
-            author: 'Michael Crichton',
-            date: 1990
+            books: [
+                {
+                    title: 'Jurassic Park',
+                    author: 'Michael Crichton',
+                    date: 1990
+                },
+                {
+                    title: 'Godzilla',
+                    author: 'Marc Cerasini',
+                    date: 1998
+                },
+                {
+                    title: 'Godzilla returns',
+                    author: 'Marc Cerasini',
+                    date: 1996
+                },
+            ]
+        }
+    },
+
+    methods: {
+        changeTitle() {
+            //console.log('You clicked me!')
+            this.books.title = 'Something else'
         }
     }
-
 }
 )
 
